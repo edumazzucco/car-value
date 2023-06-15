60986774d8a1b1322033de41a9bcccb5f30dea21
edumazzucco <edumazzucco@hotmail.com>
2023-06-15 19:47:38 -0300

    test: signIn no user w/ email, invalid pw, correct signIn

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -10,11 +10,23 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     // Mock users service
+    const users: User[] = [];
     mockUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        const filteredUsers = users.filter((user) => user.email === email);
+        return Promise.resolve(filteredUsers);
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 9999999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
+
     const module = await Test.createTestingModule({
       providers: [
         AuthService,
@@ -28,11 +40,11 @@ describe('AuthService', () => {
     service = module.get(AuthService);
   });
 
-  it('can create an instance of auth service', async () => {
+  it('should be able to create an instance of auth service', async () => {
     expect(service).toBeDefined();
   });
 
-  it('creates a new user with salted and hashed password', async () => {
+  it('should create a new user with salted and hashed password', async () => {
     const user = await service.signup('email@email.com', 'testPW');
 
     expect(user.password).not.toEqual('testPw');
@@ -41,17 +53,29 @@ describe('AuthService', () => {
     expect(hash).toBeDefined();
   });
 
-  it('throws an error if user signs up with email that is in use', async () => {
-    mockUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a@a.com', password: '1' } as User]);
+  it('should throw an error if user signs up with email that is in use', async () => {
+    await service.signup('a@a.com', '1');
     await expect(service.signup('a@a.com', '1')).rejects.toThrow(
       BadRequestException,
     );
   });
 
-  it('throws an error if there is no user with the given email', async () => {
+  it('should throw an error if there is no user with the given email', async () => {
     await expect(
       service.signin('nonuseremail@email.com', 'pass'),
     ).rejects.toThrow(NotFoundException);
+  });
+
+  it('should throw an error if an invalid password is provided', async () => {
+    await service.signup('test@test.com', 'correctPW');
+    await expect(service.signin('test@test.com', 'wrongPW')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('should return a user if correct password is provided', async () => {
+    await service.signup('correct@email.com', 'correctPW');
+    const user = await service.signin('correct@email.com', 'correctPW');
+    expect(user).toBeDefined();
   });
 });
