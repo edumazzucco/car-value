d1dd31413bf328c7d219bcdd38cd62f8dc490e27
edumazzucco <edumazzucco@hotmail.com>
2023-06-16 14:29:18 -0300

    chore: ormconfig based on process.env

---
+++ ormconfig.js
@@ -0,0 +1,25 @@
+const dbConfig = {
+  synchronize: false,
+};
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: ['**/*.entity.js'],
+    });
+    break;
+  case 'test':
+  case 'development':
+    Object.assign(dbConfig, {
+      type: 'sqlite',
+      database: 'test.sqlite',
+      entities: ['**/*.entity.ts'],
+    });
+    break;
+  case 'production':
+    break;
+  default:
+    throw new Error('Unknown environment');
+}
