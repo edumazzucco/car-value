a4a050e2340ae35d7a55349a8cf874588191098b
edumazzucco <edumazzucco@hotmail.com>
2023-06-15 22:22:47 -0300

    test: added env config manager, auth e2e signIn test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
 
-describe('Auth System (e2e)', () => {
+describe('Auth System', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -26,5 +26,22 @@ describe('Auth System (e2e)', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'email@email.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: '123456' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/current-user')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
